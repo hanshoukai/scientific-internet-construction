9285aeb615063c535382ad3c397e1defde9eb840
韩寿凯 <hankai1202@163.com>
2023-03-21 19:49:21 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update && apt install wget openssl
 mkdir hysteria && cd hysteria
-wget https://github.com/apernet/hysteria/releases/download/v1.3.3/hysteria-linux-amd64
+wget https://github.com/apernet/hysteria/releases/download/v1.3.4/hysteria-linux-amd64
 chmod +x hysteria-linux-amd64
 openssl ecparam -genkey -name prime256v1 -out ca.key
 openssl req -new -x509 -days 36500 -key ca.key -out ca.crt  -subj "/C=CN/CN=cn.bing.com"
